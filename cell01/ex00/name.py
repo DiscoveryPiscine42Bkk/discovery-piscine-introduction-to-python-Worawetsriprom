2e4546d50a44667b1c0c01c506fa91ea32b9366c
Worawetsriprom <dreamsriprom1@gmail.com>
2025-06-19 04:18:49 +0000

    python

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,3 +1,3 @@
-first_name = "Parita"
-last_name = "Prangnok"
+first_name = "Worawet"
+last_name = "Sriprom"
 print(first_name + last_name)
--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -1,4 +1,4 @@
-first_name = "Parita"
-last_name = "Prangnok"
+first_name = "Worawet"
+last_name = "sriprom"
 whole_name = first_name + " " + last_name
 print(whole_name)
